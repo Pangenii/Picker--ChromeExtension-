8738b0bf55675357b5cca90dddbf1f48eee015ba
Bishwa Pangeni <erbishwa01@gmail.com>
2026-03-12 18:46:00 +0545

    [main] : Changed a layout and copy to clipboard functions

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const button = document.getElementById("pick");
+const pickColor = document.getElementById("color");
 const result = document.getElementById("result");
+const copyButton = document.getElementById("copybutton");
+const colorInfo = document.getElementById("color-info");
 
-button.addEventListener("click", async () => {
+pickColor.addEventListener("click", async () => {
     try {
         const eyeDropper = new EyeDropper();
         const color = await eyeDropper.open();
         result.innerText = color.sRGBHex;
         await navigator.clipboard.writeText(result.innerText)
+        colorInfo.classList.remove("hidden");
     } catch (error) {
         console.error("Couldn't Pick Color!!!", error);
         result.textContent = "Couldn't Pick Color!!!"
     }
-
 });
+
+copyButton.addEventListener("click", async () => {
+    await navigator.clipboard.writeText(result.innerText)
+    copyButton.innerText = "copied";
+    setTimeout(() => {
+        copybutton.innerText = "copy";
+    }, 2000)
+})
